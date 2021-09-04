f35ea9740d8e6585b422d44467e81c26267a0367
Saurabh Kumar <89748411+Saurabh-kumar09@users.noreply.github.com>
2021-09-05 02:01:45 +0530

    Update video.py

--- video.py
+++ video.py
@@ -1,22 +1,20 @@
-
+#Content-->Video Tutorial-->How to introduce yourself
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.videoplayer import VideoPlayer
 
 
-# An App running a video file
-
 class ConFeTalk(App):
 
     def build(self):
-        self.player = VideoPlayer(source='VID-20210904-WA0021.mp4', state='play',
+        self.player = VideoPlayer(source='How To Introduce Yourself_Content- video.mp4', state='play',
                                   options={'allow_stretch': True})
 
         return (self.player)
 
 
-# Start the Video App
+# Start the Video tutorial
 
 if __name__ == '__main__':
     ConFeTalk().run()
